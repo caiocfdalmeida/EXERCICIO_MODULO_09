ad679a5ae2fbf8f4bbc8330c7192d4b8f25c0968
Caio <ccardoso.judo@gmail.com>
2023-10-02 07:50:22 -0300

    Modulo09

---
+++ EXERCICIO_MODULO_09/src/EXERCICIO/EXERCICIO_MODULO_09.java
@@ -0,0 +1,15 @@
+package EXERCICIO;
+
+public class EXERCICIO_MODULO_09 {
+	
+	public static void main (String [] args) {
+	
+	int num1 = 20;
+	System.out.println ("O número em int vale " + num1);
+	
+	long num2 = num1;
+	System.out.println ("O número em long vale " + num2);
+		
+	}
+
+}
