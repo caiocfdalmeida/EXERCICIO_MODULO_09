79485c54f756081cbe785447d4500be234920687
Caio <ccardoso.judo@gmail.com>
2023-10-02 23:24:24 -0300

    Correção de erro na atividade

--- EXERCICIO_MODULO_09/src/EXERCICIO/EXERCICIO_MODULO_09.java
+++ EXERCICIO_MODULO_09/src/EXERCICIO/EXERCICIO_MODULO_09.java
@@ -7,8 +7,8 @@ public class EXERCICIO_MODULO_09 {
 	int num1 = 20;
 	System.out.println ("O número em int vale " + num1);
 	
-	long num2 = num1;
-	System.out.println ("O número em long vale " + num2);
+	Integer num2 = num1;
+	System.out.println ("O número em Integer vale " + num2);
 		
 	}
 
